fa7896f2bea282f9c8ef61b2dc85c4ccaaac525e
hanuz06 <lee1975uz@gmail.com>
2019-11-14 02:04:14 +0000

    add bootcamp_app directory

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,34 @@
+let input = process.argv.slice(2);
+let cohortName='';
+
+if (input ){
+  cohortName = input[0];  
+}
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${cohortName}%'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+});
+
+
+
+
